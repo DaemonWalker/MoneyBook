7f7dc32cde520387409d43df19d5041e41bb3728
Daemon Walker <daemon0525@163.com>
2018-09-11 23:15:02 +0800

    更改启动端口

--- MoneyBook/Program.cs
+++ MoneyBook/Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace MoneyBook
 {
@@ -19,6 +19,7 @@ namespace MoneyBook
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            .UseUrls("http://*:80","https://*:443")
+            .UseStartup<Startup>();
     }
 }
